4b8852763071bceacbcfaa2bb46bc76563a1b85a
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 17:49:45 +0530

    Update _init_.py

--- utils/_init_.py
+++ utils/_init_.py
@@ -1,9 +1,1 @@
-from .text import (
-    plans_text,
-    welcome_text,
-    help_text,
-    payment_success_text,
-    payment_failed_text,
-    subscription_expired_text,
-    broadcast_usage_text
-)
+# Makes utils a package
