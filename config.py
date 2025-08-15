6da145d7523d1b5b364a8c8c6ab08a3048895f78
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 22:54:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,7 @@
 BOT_TOKEN = "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8"
 ADMIN_ID = 7909563220
 
-# Razorpay credentials
+# Razorpay
 RAZORPAY_KEY_ID = "rzp_test_R5a8j8yy3WEssP"
 RAZORPAY_KEY_SECRET = "DNSIjreZrmVcqsP0n6goeAoq"
 RAZORPAY_WEBHOOK_SECRET = "#Tesan25"
-
-DATABASE_URL = "sqlite:///signalbot.db"  # or PostgreSQL/MySQL if needed
