833e5489d6604c5beb41d12d4d6e1b7d1babecc5
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 21:01:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,2 +1,13 @@
-from db import Subscriber
-# Add additional models here if needed in the future
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+import datetime
+
+Base = declarative_base()
+
+class Subscriber(Base):
+    __tablename__ = "subscribers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, unique=True, index=True)
+    plan = Column(String, default="bronze")
+    expiry = Column(DateTime, default=datetime.datetime.utcnow)
