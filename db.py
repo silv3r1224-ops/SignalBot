dd2751f23163b4932ea97fd435d5df48c8062429
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 20:11:53 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+import datetime
 
-engine = create_engine(DATABASE_URL, echo=False)
+Base = declarative_base()
+engine = create_engine('sqlite:///signalbot.db', connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+class Subscriber(Base):
+    __tablename__ = "subscribers"
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
+    plan = Column(String, nullable=True)
+    subscribed_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+Base.metadata.create_all(engine)
