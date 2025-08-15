422cf0d827ae7858605ed063df7e1fe20a8b1b65
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 22:45:56 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,8 @@
+# db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import config
 
-engine = create_engine(config.DB_URL, echo=False, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(config.DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
