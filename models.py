de591471e9fc1f5733fb75cac7c4f3a6faa977e6
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 22:46:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,18 @@
-from sqlalchemy import Column, Integer, String
+# models.py
+from sqlalchemy import Column, Integer, String, Boolean, Float
 from db import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(String, unique=True, index=True)
-    plan = Column(String, default=None)
+    plan = Column(String, nullable=True)
+    subscribed = Column(Boolean, default=False)
+
+class Payment(Base):
+    __tablename__ = "payments"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    razorpay_payment_id = Column(String)
+    amount = Column(Float)
+    status = Column(String)
