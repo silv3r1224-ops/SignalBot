9c1f48020eb8bdfc1d44d068091d3c27f622a60e
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-15 20:59:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,5 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
-
-# Optional Razorpay integration
 RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
 RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET")
