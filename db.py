68e329137aeff6bbe141537e72fe638040f23807
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-16 17:57:40 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,7 @@
-# db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import config
 
-DATABASE_URL = "sqlite:///signalbot.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(config.DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
