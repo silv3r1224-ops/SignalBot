e3937051f3443cba24a351c1f5008209aae14cdc
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-16 19:54:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-import os
+# config.py
 
-ADMIN_ID = int(os.getenv("ADMIN_ID", "7909563220"))
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8")
+# Telegram Bot
+BOT_TOKEN = "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8"
+ADMIN_ID = 7909563220
 
-RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID", "rzp_test_R5a8j8yy3WEssP")
-RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET", "DNSIjreZrmVcqsP0n6goeAoq")
-RAZORPAY_WEBHOOK_SECRET = os.getenv("RAZORPAY_WEBHOOK_SECRET", "#Tesan25")
+# Razorpay
+RAZORPAY_KEY_ID = "rzp_test_R5a8j8yy3WEssP"
+RAZORPAY_KEY_SECRET = "DNSIjreZrmVcqsP0n6goeAoq"
+RAZORPAY_WEBHOOK_SECRET = "#Tesan25"
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///signalbot.db")
+# Database (for Render Postgres)
+DATABASE_URL = "sqlite:///signalbot.db"  # change to Postgres URL when deploying
