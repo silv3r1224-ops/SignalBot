6c179a0b5aabcd8e1d769322f5dc949e210c4cb5
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-16 21:28:05 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Telegram Bot Token
-TELEGRAM_TOKEN = "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8"
-
-# Razorpay API keys
-RAZORPAY_KEY_ID = "rzp_test_R5a8j8yy3WEssP"
-RAZORPAY_KEY_SECRET = "DNSIjreZrmVcqsP0n6goeAoq"
-
-# Admin Telegram ID
-ADMIN_ID = 7909563220  # Replace with your Telegram user ID
