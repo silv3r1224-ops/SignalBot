f9410ce91a4dfb66cb5b4b15e68d5f72c3c01579
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-16 18:14:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-# config.py
+class Config:
+    # Telegram
+    ADMIN_ID = 7909563220
+    BOT_TOKEN = "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8"
 
-# Telegram
-BOT_TOKEN = "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8"
-ADMIN_CHAT_ID = "7909563220"   # string is safer
+    # Razorpay
+    RAZORPAY_KEY_ID = "rzp_test_R5a8j8yy3WEssP"
+    RAZORPAY_KEY_SECRET = "DNSIjreZrmVcqsP0n6goeAoq"
+    RAZORPAY_WEBHOOK_SECRET = "#Tesan25"
 
-# Razorpay
-RAZORPAY_KEY_ID = "rzp_test_R5a8j8yy3WEssP"
-RAZORPAY_KEY_SECRET = "DNSIjreZrmVcqsP0n6goeAoq"
-RAZORPAY_WEBHOOK_SECRET = "#Tesan25"
+    # Database (SQLite local file)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///bot.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Public URL for webhook (Render URL)
-PUBLIC_URL = "https://signalbot-tfnb.onrender.com"
-
-# Database
-DATABASE_URL = "sqlite:///data.db"
+    # Webhook URL
+    WEBHOOK_URL = "https://signalbot-tfnb.onrender.com/telegram-webhook"
