b7de0a012e5b1aca274dd2e86928f46ad9e2e743
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-16 18:27:34 +0530

    Delete db.py

--- db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import config
-
-engine = create_engine(config.DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
