9c9dfe7a218e386dfe74092312e96ce9f02b7589
silv3r1224-ops <silv3r1224@gmail.com>
2025-08-16 21:30:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import threading
 TELEGRAM_TOKEN = "8250035384:AAHbSjhqT0fODfQHnjgBFcZLuIQeaUBJeP8"
 RAZORPAY_KEY_ID = "rzp_test_R5a8j8yy3WEssP"
 RAZORPAY_KEY_SECRET = "DNSIjreZrmVcqsP0n6goeAoq"
-ADMIN_ID = 123456789  # Replace with your Telegram ID
+ADMIN_ID = 7909563220  # Replace with your Telegram ID
 
 SUBSCRIBERS_FILE = "subscribers.json"
 PLANS_FILE = "plans.json"
